794b1f8c7ac477d68823dc75e656d1d7f8bde446
Acer <Acer@DESKTOP-HJEFQV5>
2022-10-16 18:39:14 +0530

    First Commit

---
+++ B28/src/org/tns/INHERITENCE/Single_inheritence.java
@@ -0,0 +1,10 @@
+package org.tns.INHERITENCE;
+
+public class Single_inheritence {
+	
+	public static void main(String[] args) {
+		Withdraw ob=new Withdraw();
+		System.out.println("the total amount = "+ob.total_savings);
+		System.out.println("amount withdraw = "+ob.Withdraw);
+	}
+}
---
+++ B28/src/org/tns/INHERITENCE/Withdraw.java
@@ -0,0 +1,11 @@
+package org.tns.INHERITENCE;
+
+public class Withdraw {
+
+	int Withdraw=1000;
+	int total_savings=5000;
+	public static void main(String[] args) {
+		Withdraw ob=new Withdraw();
+		System.out.println("amount withdraw "+ob.Withdraw);
+	}
+}
